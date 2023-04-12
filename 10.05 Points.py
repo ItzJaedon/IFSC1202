6c2b75959cbce626441197fc7b2ca2dc01f05f85
ItzJaedon <jwilliams8@ualr.edu>
2023-04-12 22:32:54 +0000

    .

--- 10.05 Points.py
+++ 10.05 Points.py
@@ -1,41 +1,27 @@
-from math import sqrt, atan, pi
-
-# Step 1 - Define the class object "Point"
+import math
 class Point:
-    # Step 2 - Define the initializer and any default values
-    def __init__(self, Xvalue, Yvalue):
-        # Step 3 - Define the object attributes
-        self.x = Xvalue
-        self.y = Yvalue
-        
-    # Step 4 - Define the methods for the object
-    # ToString returns a nicely formated string to represent the data point
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y   
     def ToString(self):
         return "(" + str(self.x) + ", " + str(self.y) + ")"
-
-# Distance function
-def Distance(PointA, PointB):
-    distance = sqrt((PointB.x - PointA.x) ** 2 + (PointB.y - PointA.y) ** 2)
+def Distance(pointA, pointB):
+    distance = math.sqrt((pointB.x - pointA.x)**2 + (pointB.y - pointA.y)**2)
     return distance
-
-# MidPoint function
-def MidPoint(PointA, PointB):
-    midpoint_x = (PointB.x + PointA.x) / 2
-    midpoint_y = (PointB.y + PointA.y) / 2
-    midpoint = Point(midpoint_x, midpoint_y)
-    return midpoint
-
-# XAngle function
-def XAngle(PointA, PointB):
-    slope = (PointB.y - PointA.y) / (PointB.x - PointA.x)
-    xangle = atan(slope) * 180 / pi
-    return xangle
-print("{:<18} {:<18} {:<12} {:<18} {:<8}".format("Point A", "Point B", "Distance", "Midpoint", "XAngle"))
+def MidPoint(pointA, pointB):
+    x = (pointB.x + pointA.x) / 2
+    y = (pointB.y + pointA.y) / 2
+    return Point(x, y)
+def XAngle(pointA, pointB):
+    slope = (pointB.y - pointA.y) / (pointB.x - pointA.x)
+    angle = math.atan(slope) * 180 / math.pi
+    return angle
 with open("10.05 Points.txt", "r") as f:
     for line in f:
-        data = line.strip().split(",")
-        # Create two Point objects
-        PointA = Point(float(data[0]), float(data[1]))
-        PointB = Point(float(data[2]), float(data[3]))
-print("-" * 78)
-print("{:<18} {:<18} {:<12.2f} {:<18} {:<8.2f}".format(PointA.ToString(), PointB.ToString(), distance, midpoint.ToString(), xangle))
+        data = line.split(",")
+        pointA = Point(float(data[0].replace(",", ".")), float(data[1].replace(",", ".")))
+        pointB = Point(float(data[2].replace(",", ".")), float(data[3].replace(",", ".")))          
+        distance = Distance(pointA, pointB)
+        midpoint = MidPoint(pointA, pointB)
+        xangle = XAngle(pointA, pointB)
+        print("{0:<20}{1:<20}{2:<20}{3:<20}{4:<20}".format(pointA.ToString(), pointB.ToString(), round(distance, 7), midpoint.ToString(), round(xangle, 7)))
