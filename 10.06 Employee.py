e0c110edc84cc2b456639b29a6726fc139b3fada
ItzJaedon <jwilliams8@ualr.edu>
2023-04-12 22:25:37 +0000

    .

--- 10.06 Employee.py
+++ 10.06 Employee.py
@@ -1,30 +1,22 @@
 class Employee:
-    def __init__(self, FirstName, LastName, IDNumber, HoursWorked, Wage):
-        self.FirstName = FirstName
-        self.LastName = LastName
-        self.IDNumber = IDNumber
-        self.HoursWorked = HoursWorked
-        self.Wage = Wage
-class Employee:
-    def __init__(self, FirstName, LastName, IDNumber, HoursWorked, Wage):
-        self.FirstName = FirstName
-        self.LastName = LastName
-        self.IDNumber = IDNumber
-        self.HoursWorked = HoursWorked
-        self.Wage = Wage
+    def __init__(self, first_name, last_name, id_number, hours_worked, wage):
+        self.FirstName = first_name
+        self.LastName = last_name
+        self.IDNumber = id_number
+        self.HoursWorked = hours_worked
+        self.Wage = wage
     
     def WeeklyPay(self):
         if self.HoursWorked <= 40:
             return self.HoursWorked * self.Wage
         else:
             regular_pay = 40 * self.Wage
-            overtime_pay = (self.HoursWorked - 40) * self.Wage * 1.5
+            overtime_pay = (self.HoursWorked - 40) * 1.5 * self.Wage
             return regular_pay + overtime_pay
-employees = []
-with open('10.06 Payroll.txt', 'r') as file:
-    for line in file:
+with open('10.06 Payroll.txt') as f:
+    for line in f:
         data = line.strip().split(',')
-        emp = Employee(data[0], data[1], data[2], int(data[3]), float(data[4]))
-        employees.append(emp)
-for emp in employees:
-    print(f"{emp.FirstName} {emp.LastName}, ID: {emp.IDNumber}, Hours Worked: {emp.HoursWorked}, Hourly Wage: ${emp.Wage:.2f}, Weekly Pay: ${emp.WeeklyPay():.2f}")
+        first_name, last_name, id_number, hours_worked, wage = data
+        employee = Employee(first_name, last_name, id_number, float(hours_worked), float(wage))
+        weekly_pay = employee.WeeklyPay()
+        print('{:8s} {:8s} {:7s} {:7.2f} {:7.2f} {:7.2f}'.format(first_name, last_name, id_number, float(hours_worked), float(wage), weekly_pay))
