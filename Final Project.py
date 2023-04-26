84b7300ce18b28c3a0e1102f2f760c726e48d74e
ItzJaedon <jwilliams8@ualr.edu>
2023-04-26 07:04:31 +0000

    .

--- Final Project.py
+++ Final Project.py
@@ -1,39 +1,71 @@
-class VendingMachine:
-    def __init__(self, filename):
-        self.products = []
-        self.prices = {}
-        self.inventory = {}
+class VendingItem:
+    def __init__(self, name, initial_count, cost_per_item):
+        self.Name = name
+        self.InitialCount = initial_count
+        self.CostPerItem = cost_per_item
+        self.SoldCount = 0
+        self.LostCount = 0
+
+    def InitialValue(self):
+        return self.InitialCount * self.CostPerItem
+
+    def SoldValue(self):
+        return self.SoldCount * self.CostPerItem
+
+    def LostValue(self):
+        return self.LostCount * self.CostPerItem
+
+
+class Vending:
+    def __init__(self):
+        self.VendingList = []
         self.VendingTotalInitialValue = 0
         self.VendingTotalInitialCount = 0
-        with open(filename, 'r') as file:
-            for line in file:
-                data = line.strip().split(',')
-                product_name = data[0]
-                price = float(data[1])
-                quantity = int(data[2])
-                self.add_product(product_name, price, quantity)
-    def add_product(self, product_name, price, quantity):
-        self.products.append(product_name)
-        self.prices[product_name] = price
-        self.inventory[product_name] = quantity
-    def buy_product(self, product_name):
-        if product_name not in self.products:
-            print(f"Error: {product_name} not found in vending machine")
-            return
-        if self.inventory[product_name] == 0:
-            print(f"Error: {product_name} is out of stock")
-            return
-        self.inventory[product_name] -= 1
-        self.VendingTotalInitialValue += self.prices[product_name]
-        self.VendingTotalInitialCount += 1
-        print(f"Thank you for buying {product_name} for ${self.prices[product_name]:.2f}. "
-              f"There are {self.inventory[product_name]} {product_name}(s) left.")
+        self.VendingTotalSoldValue = 0
+        self.VendingTotalSoldCount = 0
+        self.VendingTotalLostValue = 0
+        self.VendingTotalLostCount = 0
+
+    def load_vending_items_from_file(self, filename):
+        with open(filename, "r") as f:
+            for line in f:
+                name, initial_count, cost_per_item = line.split()
+                item = VendingItem(name, int(initial_count), float(cost_per_item))
+                self.VendingTotalInitialValue += item.InitialValue()
+                self.VendingTotalInitialCount += item.InitialCount
+                self.VendingList.append(item)
+
     def print_vending(self):
-        print(f"{'Product':<12} {'Price':<8} {'Quantity':<10}")
-        for product_name in self.products:
-            print(f"{product_name:<12} ${self.prices[product_name]:.2f} {self.inventory[product_name]:<10}")
-        print(f"{'Total':<8} {self.VendingTotalInitialCount:<11} ${'' if self.VendingTotalInitialValue == 0 else self.VendingTotalInitialValue:.2f}")
-vending_machine = VendingMachine('Final Project Vending.txt')
-vending_machine.print_vending()
-vending_machine.buy_product('Coke')
+        print("{:<10} {:<15} {:<15} {:<15} {:<15} {:<15} {:<15}".format("Name", "Initial Count", "Price Per Item", "Sold Count", "Sold Value", "Lost Count", "Lost Value"))
+        for item in self.VendingList:
+            print("{:<10} {:<15} ${:<14} {:<15} ${:<15} {:<15} ${:<15}".format(item.Name, item.InitialCount, item.CostPerItem, item.SoldCount, item.SoldValue(), item.LostCount, item.LostValue()))
+        print("\n{:<26} ${:<14} {:<15} ${:<15} {:<15} ${:<15}".format("Total", self.VendingTotalInitialValue, self.VendingTotalInitialCount, self.VendingTotalSoldValue, self.VendingTotalLostCount, self.VendingTotalLostValue))
+
+    def find_product(self, producttofind):
+        for i, item in enumerate(self.VendingList):
+            if item.Name == producttofind:
+                return i
+        return None
+
+    def update_vending(self, productname):
+        index = self.find_product(productname)
+        if index is not None:
+            item = self.VendingList[index]
+            if item.SoldCount < item.InitialCount:
+                item.SoldCount += 1
+                self.VendingTotalSoldCount += 1
+                self.VendingTotalSoldValue += item.CostPerItem
+            else:
+                item.LostCount += 1
+                self.VendingTotalLostCount += 1
+                self.VendingTotalLostValue += item.CostPerItem
+
+
+# Main program
+vending_machine = Vending()
+vending_machine.load_vending_items_from_file("Final Project Vending.txt")
+with open("Final Project Sales.txt", "r") as f:
+    for line in f:
+        product = line.strip()
+        vending_machine.update_vending(product)
 vending_machine.print_vending()
